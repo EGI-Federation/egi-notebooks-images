e4d9021c227375f687825dd7a1d5ca61435cf9a0
Enol Fernández <enol.fernandez@egi.eu>
2020-08-31 13:21:53 +0100

    Make tests fail whenever they do fail (#15)

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -ueo pipefail
+
 # test notebooks in these directories
 IMGS="single-user single-user-d4science"
 
 for img in $IMGS ; do
     for f in $img/tests/*.ipynb; do
-        [ -e "$f" ] \
-            && docker run -v $PWD/$img/tests:/tests $DOCKER_REPO/$img \
-                jupyter nbconvert --to rst --execute --stdout /tests/$(basename $f) \
-            || true
+        if [ -e "$f" ]; then
+            docker run -v $PWD/$img/tests:/tests $DOCKER_REPO/$img \
+                jupyter nbconvert --to rst --execute --stdout /tests/$(basename $f)
+        else
+            true
+        fi
     done
 done
