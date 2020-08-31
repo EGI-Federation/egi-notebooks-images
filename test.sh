548b8670e9c5cb319f8ab5ec9bed05bdb91d239c
Enol Fernandez <enol.fernandez@egi.eu>
2020-08-31 10:45:12 +0100

    Do not mix tabs and spaces

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ IMGS="single-user single-user-d4science"
 
 for img in $IMGS ; do
     for f in $img/tests/*.ipynb; do
-	[ -e "$f" ] \
+        [ -e "$f" ] \
             && docker run -v $PWD/$img/tests:/tests $DOCKER_REPO/$img \
                 jupyter nbconvert --to rst --execute --stdout /tests/$(basename $f) \
-	    || true
+            || true
     done
 done
