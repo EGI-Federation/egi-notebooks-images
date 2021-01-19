c8b8e20d0da7d1216f2052ac8e4110516a6277b6
Andrea Manzi <andrea.manzi@gmail.com>
2021-01-19 15:38:52 +0100

    add a workaround to avoid deploy script to timeout (#36)
    * hack to avoid timeout when deploying the images
    
    * remove the travis_wait option on deploy

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,19 @@ TAG=$2
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push $DOCKER_REPO/base
-docker push $DOCKER_REPO/base:$TAG
-docker push $DOCKER_REPO/single-user
-docker push $DOCKER_REPO/single-user:$TAG
-docker push $DOCKER_REPO/single-user-d4science
-docker push $DOCKER_REPO/single-user-d4science:$TAG
-docker push $DOCKER_REPO/d4science-storage
-docker push $DOCKER_REPO/d4science-storage:$TAG
+docker push $DOCKER_REPO/base &
+docker push $DOCKER_REPO/base:$TAG &
+docker push $DOCKER_REPO/single-user &
+docker push $DOCKER_REPO/single-user:$TAG &
+docker push $DOCKER_REPO/single-user-d4science &
+docker push $DOCKER_REPO/single-user-d4science:$TAG &
+docker push $DOCKER_REPO/d4science-storage &
+docker push $DOCKER_REPO/d4science-storage:$TAG &
 
+# Output to the screen every 9 minutes to prevent a travis timeout
+#see https://stackoverflow.com/questions/39288834/travis-script-deployment-timeout
+export PID=$!
+while [[ `ps -p $PID | tail -n +2` ]]; do
+  echo 'Deploying'
+  sleep 540
+done
