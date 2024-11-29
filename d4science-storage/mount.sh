7a6b31b578ec54cac54e1e45eeb9b0695b5e912c
Enol Fernández <enol.fernandez@egi.eu>
2024-11-29 12:12:00 +0000

    Update the fuse integration jar (#225)
    * Update the fuse integration jar
    
    * Do I hate the linter?

--- d4science-storage/mount.sh
+++ d4science-storage/mount.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 FUSE_JAR="/sh-fuse-integration-jar-with-dependencies.jar"
-CONTEXT="/d4science.research-infrastructures.eu"
 
 echo "Mounting filesystem at $MNTPATH"
 
-java -jar "$FUSE_JAR" "$D4SCIENCE_TOKEN" "$CONTEXT" "$MNTPATH"
+java -jar "$FUSE_JAR" "$D4SCIENCE_TOKEN" "$MNTPATH"
